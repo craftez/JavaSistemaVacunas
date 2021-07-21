e152c27bb5ae4f4527b616e1cb621b75e1202b62
Steven Herrera <sherrerac215@ulacit.ed.cr>
2021-07-21 16:43:41 -0600

    Cambios de case en LibroDeVacunas

--- src/models/LibroDeVacunas.java
+++ src/models/LibroDeVacunas.java
@@ -15,6 +15,53 @@ public class LibroDeVacunas {
     private String fechaDePrimeraDosis;
     private String fechaDeSegundaDosis;
     private boolean esquemaCompleto;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Vacuna getVacuna() {
+        return vacuna;
+    }
+
+    public String getFechaDePrimeraDosis() {
+        return fechaDePrimeraDosis;
+    }
+
+    public String getFechaDeSegundaDosis() {
+        return fechaDeSegundaDosis;
+    }
+
+    public boolean isEsquemaCompleto() {
+        return esquemaCompleto;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setVacuna(Vacuna vacuna) {
+        this.vacuna = vacuna;
+    }
+
+    public void setFechaDePrimeraDosis(String fechaDePrimeraDosis) {
+        this.fechaDePrimeraDosis = fechaDePrimeraDosis;
+    }
+
+    public void setFechaDeSegundaDosis(String fechaDeSegundaDosis) {
+        this.fechaDeSegundaDosis = fechaDeSegundaDosis;
+    }
+
+    public void setEsquemaCompleto(boolean esquemaCompleto) {
+        this.esquemaCompleto = esquemaCompleto;
+    }
+
+    @Override
+    public String toString() {
+        return "LibroDeVacunas{" + "nombre=" + nombre + ", vacuna=" + vacuna + ", fechaDePrimeraDosis=" + fechaDePrimeraDosis + ", fechaDeSegundaDosis=" + fechaDeSegundaDosis + ", esquemaCompleto=" + esquemaCompleto + '}';
+    }
+    
+    
     
     public void Vacunar()
     {
